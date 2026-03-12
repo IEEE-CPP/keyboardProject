4d070833ff41e4f87d0fea4cd783a40fc55c3f63
drnfc <djbatfrog@gmail.com>
2026-03-12 12:20:42 -0700

    initial commit

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    devshell.url = "github:numtide/devshell";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devshell.flakeModule
+      ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          devshells.default = {
+            packages = with pkgs; [
+              kicad
+            ];
+          };
+          packages.default = pkgs.kicad;
+        };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+
+      };
+    };
+}
